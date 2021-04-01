dc85cd0e6e67cda538e66d9261872e1b4bdb4bc6
Mario Arriola <64659499+ARRIOLALEO@users.noreply.github.com>
2021-04-01 14:53:36 +0600

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,13 @@
+firstNumber = input(" inset the first number ")
+secondNumber = input ("insert the second Numbers ")
+if firstNumber < secondNumber:
+    print(f"this is the biggest {secondNumber}")
+else:
+    print(f"this is the bigget {firstNumber}")
+counter = 0
+while counter < 2:
+    print(f"{counter}")
+    counter += 1
+
+for i in [0,1,2,3]:
+    print(f"{i}")
